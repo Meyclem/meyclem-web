5e92c79512b8d25d7061a869b41c36efc59e1a72
Clement <meyclem@gmail.com>
2021-06-12 11:10:14 +0200

    add meta and contact info (#10)
    ## Description
    
    This PR adds contact information and basic meta tags.

--- src/components/layout/SEO.tsx
+++ src/components/layout/SEO.tsx
@@ -7,6 +7,17 @@ export const SEO: React.FC<{ title: string }> = ({ title }) => (
       {title}
     </title>
     <link rel="icon" type="image/png" href="/images/favicon.svg" />
+    <meta name="description" content="Clement Meyer, Fullstack web developer" />
+    <meta property="og:title" content="Meyclem" />
+    <meta
+      property="og:description"
+      content="Clement Meyer, Fullstack web developer"
+    />
+    <meta
+      property="og:image"
+      content="https://www.meyclem.com/images/meta-image.png"
+    />
+    <meta property="og:url" content="https://www.meyclem.com/" />
     <meta charSet="utf-8" />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
   </Head>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
 import { Layout } from "@components/layout";
 
+const contacts = [
+  { title: "github", href: "https://github.com/meyclem" },
+  {
+    title: "linkedin",
+    href: "https://www.linkedin.com/in/clement-meyer-2715b554/",
+  },
+  { title: "email", href: "mailto:meyclem@gmail.com" },
+];
+
 const Index = (): JSX.Element => {
   return (
     <Layout title="About">
@@ -11,10 +20,23 @@ const Index = (): JSX.Element => {
             alt="Clement's picture"
           />
           <div>
-            <h1 className="font-bold text-3xl">
+            <h1 className="font-bold text-3xl mb-6">
               Hi! I'm <span className="text-pink-400">Clement</span>
             </h1>
-            <h2 className="font-semibold text-xl">Fullstack web developer</h2>
+            <h2 className="font-semibold text-xl mb-4">
+              Fullstack web developer
+            </h2>
+            <div className="flex opacity-50">
+              {contacts.map((contact) => (
+                <a href={contact.href} target="blank" key={contact.title}>
+                  <img
+                    src={`/images/${contact.title}.svg`}
+                    className="h-6 w-6 mr-2 last:mr-0"
+                    alt={`${contact.title} icon`}
+                  />
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
