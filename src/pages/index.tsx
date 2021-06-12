668901308175afcc8493757d6dbd733f2e9b1b74
meyer clement <meyclem@gmail.com>
2021-06-12 10:08:03 +0200

    clean home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,89 +4,19 @@ const Index = (): JSX.Element => {
   return (
     <Layout title="About">
       <div className="container mx-auto">
-        <div>
-          <p className="font-bold">
-            Hi! I'm <span className="text-pink-400">Clement</span>
-          </p>
-          <p>Fullstack web developer</p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
-          <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            dicta ipsum. Praesentium temporibus quo debitis, placeat accusantium
-            tempora eligendi reprehenderit molestiae rerum ut? Harum quidem
-            magnam ad a. Aliquam, omnis!
-          </p>
+        <div className="flex items-center h-72 justify-center">
+          <img
+            className="rounded-full w-32 h-32 mr-16"
+            src="https://avatars.githubusercontent.com/u/29123047?v=4"
+            alt="Clement's picture"
+          />
+          <div>
+            <h1 className="font-bold text-3xl">
+              Hi! I'm <span className="text-pink-400">Clement</span>
+            </h1>
+            <h2 className="font-semibold text-xl">Fullstack web developer</h2>
+          </div>
         </div>
-        <img
-          className="rounded-full w-32 h-32"
-          src="https://avatars.githubusercontent.com/u/29123047?v=4"
-          alt=""
-        />
       </div>
     </Layout>
   );
